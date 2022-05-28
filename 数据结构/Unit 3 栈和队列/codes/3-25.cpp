2660fdcfec2a08957f8109d1e0629933369dedd4
shinehale <3116227229@qq.com>
2022-05-28 20:53:43 +0800

    (Data structure):finish Unit 4 codes

--- 数据结构/Unit 3 栈和队列/codes/3-25.cpp
+++ 数据结构/Unit 3 栈和队列/codes/3-25.cpp
@@ -1,5 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+int f ( int x ){
+	if ( x == 0 ) return 1;
+	else return x * f( x/2 );
+}
 int main(){
-	
+	int n; cin>> n;
+	cout << f ( n );
+	return 0;
 }
